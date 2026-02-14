0e5e4aa152ec169e4f3bb0e1834e598fd90ab852
Anejaprachi64 <122356899+Anejaprachi64@users.noreply.github.com>
2026-02-14 20:22:38 +0530

    Clean up AuthCallback component by removing comments
    Removed commented-out code for the AuthCallback component.

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -1,24 +1,4 @@
-// "use client";
 
-// import { useEffect } from "react";
-// import { supabase } from "@/lib/supabaseClient";
-// import { useRouter } from "next/navigation";
-
-// export default function AuthCallback() {
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     supabase.auth.getSession().then(() => {
-//       router.push("/");
-//     });
-//   }, [router]);
-
-//   return (
-//     <div className="min-h-screen flex items-center justify-center">
-//       Logging you in...
-//     </div>
-//   );
-// }
 "use client";
 
 import { useEffect } from "react";
